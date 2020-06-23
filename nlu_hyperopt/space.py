da51fdae27b362a5e6961c3a52158b3e44605f70
bdai <bdai_decathlon_windows>
2020-06-23 11:00:16 +0800

    update

--- nlu_hyperopt/space.py
+++ nlu_hyperopt/space.py
@@ -8,4 +8,4 @@ from hyperopt import hp
 # }
 
 # Default search space: Try different numbers of training epochs.
-search_space = {"epochs": hp.qloguniform("epochs", 0, 4, 2)}
+search_space = {"epochs": hp.qloguniform("epochs", 0, 4.7, 2)}
